a1ef5911a30b974a97feddb0228e569184f8014b
ASUS <ASUS@DESKTOP-H53P1TQ>
2022-09-17 14:33:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstConsoleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
